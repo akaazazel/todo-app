1c3fde86de5f7ecb2eee300f4db90b150b9f23f2
akaazazel <cvakshay764@gmail.com>
2025-09-06 02:01:29 +0530

    style fix: header and input box position fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,94 @@ import TodoInput from "./components/TodoInput";
 import { Todo } from "./todo_function";
 
 function App() {
-    const testData = [
-        new Todo({
-            todo_id: "mf5adwv45qg24dh1",
-            title: "Wash dishes",
-            description: "This is a wash dishes description",
-            date_created: new Date(),
-            is_done: false,
-        }),
-        new Todo({
-            todo_id: "mf52dwv45qg24dh1",
-            title: "Clean clothes",
-            description: "This is a clean clothes description",
-            date_created: new Date(),
-            is_done: false,
-        }),
-    ];
+    // const testData = [
+    //     new Todo({
+    //         todo_id: "mf5adwv45qg24dh1",
+    //         title: "Wash dishes",
+    //         description: "This is a wash dishes description",
+    //         date_created: new Date(),
+    //         is_done: false,
+    //     }),
+    //     new Todo({
+    //         todo_id: "mf52dwv45qg24dh1",
+    //         title: "Clean clothes",
+    //         description: "This is a clean clothes description",
+    //         date_created: new Date(),
+    //         is_done: false,
+    //     }),
+    //     new Todo({
+    //         todo_id: "mf5adwv45qg24dh1",
+    //         title: "Wash dishes",
+    //         description: "This is a wash dishes description",
+    //         date_created: new Date(),
+    //         is_done: false,
+    //     }),
+    //     new Todo({
+    //         todo_id: "mf52dwv45qg24dh1",
+    //         title: "Clean clothes",
+    //         description: "This is a clean clothes description",
+    //         date_created: new Date(),
+    //         is_done: false,
+    //     }),
+    //     new Todo({
+    //         todo_id: "mf5adwv45qg24dh1",
+    //         title: "Wash dishes",
+    //         description: "This is a wash dishes description",
+    //         date_created: new Date(),
+    //         is_done: false,
+    //     }),
+    //     new Todo({
+    //         todo_id: "mf52dwv45qg24dh1",
+    //         title: "Clean clothes",
+    //         description: "This is a clean clothes description",
+    //         date_created: new Date(),
+    //         is_done: false,
+    //     }),
+    //     new Todo({
+    //         todo_id: "mf5adwv45qg24dh1",
+    //         title: "Wash dishes",
+    //         description: "This is a wash dishes description",
+    //         date_created: new Date(),
+    //         is_done: false,
+    //     }),
+    //     new Todo({
+    //         todo_id: "mf52dwv45qg24dh1",
+    //         title: "Clean clothes",
+    //         description: "This is a clean clothes description",
+    //         date_created: new Date(),
+    //         is_done: false,
+    //     }),
+    //     new Todo({
+    //         todo_id: "mf5adwv45qg24dh1",
+    //         title: "Wash dishes",
+    //         description: "This is a wash dishes description",
+    //         date_created: new Date(),
+    //         is_done: false,
+    //     }),
+    //     new Todo({
+    //         todo_id: "mf52dwv45qg24dh1",
+    //         title: "Clean clothes",
+    //         description: "This is a clean clothes description",
+    //         date_created: new Date(),
+    //         is_done: false,
+    //     }),
+    //     new Todo({
+    //         todo_id: "mf5adwv45qg24dh1",
+    //         title: "Wash dishes",
+    //         description: "This is a wash dishes description",
+    //         date_created: new Date(),
+    //         is_done: false,
+    //     }),
+    //     new Todo({
+    //         todo_id: "mf52dwv45qg24dh1",
+    //         title: "Clean clothes",
+    //         description: "This is a clean clothes description",
+    //         date_created: new Date(),
+    //         is_done: false,
+    //     }),
+    // ];
 
-    const [todosList, setTodosList] = useState(testData);
+    const [todosList, setTodosList] = useState([]);
 
     const [editingTodo, setEditingTodo] = useState(null);
     const [editingState, setEditingState] = useState(false);
